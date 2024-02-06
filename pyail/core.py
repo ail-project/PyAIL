b2522184273af91c4be5c06fbe9f3542e30e2707
terrtia <or1994@hotmail.fr>
2024-02-06 11:23:46 +0100

    fix: [core] feeder data type

--- pyail/core.py
+++ pyail/core.py
@@ -9,13 +9,17 @@ from uuid import UUID
 
 # # TODO: add exception
 def encode_and_compress_data(data):
-    return base64.b64encode(gzip.compress(data.encode('utf-8'))).decode()
+    if not isinstance(data, bytes):
+        data = data.encode('utf-8')
+    return base64.b64encode(gzip.compress(data)).decode()
 
 # # TODO: add exception
 # # TODO: add encoding
 def get_data_sha256(data):
+    if not isinstance(data, bytes):
+        data = data.encode('utf-8')
     m = sha256()
-    m.update(data.encode('utf-8'))
+    m.update(data)
     return m.hexdigest()
 
 # # TODO: ADD NEW AIL OBJECT
